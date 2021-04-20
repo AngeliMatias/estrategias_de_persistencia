8ed851591ce1d0075ca8e57496f936c04941a926
AngeliM96 <matiasangeli@gmail.com>
2021-04-20 20:30:29 -0300

    Creación de las tablas de clase 1

---
+++ clase1practica.sql
@@ -0,0 +1,20 @@
+CREATE TABLE departamentos(
+    codDpto VARCHAR(4) NOT NULL,
+    nombreDepto VARCHAR(20),
+    ciudad VARCHAR(15),
+    codDirector VARCHAR(12),
+    PRIMARY KEY(`codDpto`)
+); CREATE TABLE empleados(
+    ndiEMP VARCHAR(12) NOT NULL,
+    nomEmp VARCHAR(30),
+    sexEmp CHAR(1),
+    fecNac DATE,
+    fecIncorporacion DATE,
+    salEmp FLOAT,
+    comisionE FLOAT,
+    cargoE VARCHAR(15),
+    jefeID VARCHAR(12),
+    codDepto VARCHAR(4),
+    PRIMARY KEY(`ndiEMP`),
+    FOREIGN KEY(codDepto) REFERENCES departamentos(codDpto)
+);
