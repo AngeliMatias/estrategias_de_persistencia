dc5d97ca828b5fbde29f0c2b140169ea4d75985f
AngeliM96 <matiasangeli@gmail.com>
2021-04-29 19:17:46 -0300

    Ejercicios hechos al d[ia 29/04

--- Clase 1/clase1_creacion_estructura.sql
+++ Clase 1/clase1_creacion_estructura.sql
@@ -1,9 +1,13 @@
+CREATE persistencia_clase1;
+-- Selección de la base de datos
+USE persistencia_clase1;
+-- Creación de las tablas
 CREATE TABLE departamentos(
     codDpto VARCHAR(4) NOT NULL,
     nombreDepto VARCHAR(20),
     ciudad VARCHAR(15),
     codDirector VARCHAR(12),
-    PRIMARY KEY(`codDpto`)
+    PRIMARY KEY(codDpto)
 ); CREATE TABLE empleados(
     ndiEMP VARCHAR(12) NOT NULL,
     nomEmp VARCHAR(30),
